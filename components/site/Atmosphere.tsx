22ff901d2306d3dab3a4b5ed9f7a03ac75a2366c
arbyter-os <arbyteros@gmail.com>
2026-09-26 11:39:44 +0530

    feat: add blue black white starfield atmosphere

---
+++ components/site/Atmosphere.tsx
@@ -0,0 +1,10 @@
+export function Atmosphere() {
+  return (
+    <div className="site-atmosphere" aria-hidden="true">
+      <div className="site-atmosphere-glow" />
+      <div className="site-atmosphere-stars site-atmosphere-stars-a" />
+      <div className="site-atmosphere-stars site-atmosphere-stars-b" />
+      <div className="site-atmosphere-stars site-atmosphere-stars-c" />
+    </div>
+  );
+}
